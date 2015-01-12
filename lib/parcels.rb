5d90d9005ce2115e6637d51cb85d7e7510032ab9
Ben Pritchard & Lee Jorgensen <student@epicodus.com>
2015-01-12 14:30:16 -0800

    add cost_to_ship method

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -3,9 +3,17 @@ class Parcel
     @length = length
     @height = height
     @width = width
+    @volume = @length * @height * @width
+    @shipping_cost = 0.01
   end
 
   define_method(:volume) do
-    @volume = @length * @height * @width
+    @volume
+  end
+
+  define_method(:cost_to_ship) do
+    @shipping_cost = @volume * 2.00 / 100.00
+
+    "$" + sprintf("%.2f", @shipping_cost)
   end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -1,11 +1,19 @@
 require ('rspec')
 require ('parcels')
+require ('pry')
 
 describe(Parcel) do
   describe('#volume') do
     it('takes values from user and returns the volume') do
       test_parcel = Parcel.new(3, 4, 5)
       expect(test_parcel.volume()).to(eq(60))
+    end
+  end
+
+  describe('#cost_to_ship') do
+    it('returns the cost to ship based on volume') do
+      test_parcel = Parcel.new(3, 4, 5)
+      expect(test_parcel.cost_to_ship()).to(eq("$1.20"))
     end
   end
 end
